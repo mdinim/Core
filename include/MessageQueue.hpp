afed1775e3cff1adbdad5eefc50d779774a1befd
Dániel Molnár <mdinim@gmail.com>
2019-03-06 19:41:08 +0100

    Docs for MessageQueue

--- include/MessageQueue.hpp
+++ include/MessageQueue.hpp
@@ -11,49 +11,78 @@
 
 namespace Core {
 
+/// \brief \enum for Message Priority
+/// Any number can be used for arbitrary priority, these are just reference numbers
+/// So users of the construct have the ability to use them.
 enum class MessagePriority : unsigned {
     Low = 0,
     Normal = 50,
     High = 100
 };
 
+/// \brief Message queue template class.
+/// Thread safe implementation of a message queue.
+/// Any type can be passed through it.
 template<class MessageContent>
 class MessageQueue {
 private:
+    /// \brief Message wrapper (internal) type.
+    /// Wraps the MessageContent type with its dispatched priority.
     struct MessageType {
+        /// \brief Holds the priority of the message
         unsigned priority;
+
+        /// \brief Holds the content of the message
         MessageContent content;
 
+        /// \brief Construct the wrapper type.
+        /// \param priority holds the priority value of the message.
+        /// \param args holds the constructor arguments for the wrapped type.
         template<class ...Args>
         MessageType(unsigned priority, Args &&... args) : priority(priority),
                                                           content(std::forward<Args>(args)...) {}
 
+        /// \brief Comparison operator for the underlying priority queue.
         bool operator<(const MessageType &rhs) const {
             return priority < rhs.priority;
         }
 
+        /// \brief Equality operator for the unterlying priority queue.
         bool operator==(const MessageType &rhs) const {
             return priority == rhs.priority;
         }
     };
 
+    /// \brief Guards the priority queue.
     std::mutex _queueGuard;
+
+    /// \brief Condition variable to have the ability for users to wait for the next message.
     std::condition_variable conditionVariable;
+
+    /// \brief Container of the messages.
     std::priority_queue<MessageType> _queue;
 public:
+    /// \brief Default construct the empty priority queue.
     MessageQueue() = default;
 
+    /// \brief Disabled copy constructor.
     MessageQueue(const MessageQueue &) = delete;
 
+    /// \brief Disabled copy-assignment operator.
+    MessageType& operator=(const MessageQueue &) = delete;
+
+    /// \brief Check if the queue is empty.
     bool empty() const {
         return _queue.empty();
     }
 
+    /// \brief Queue messages with default priority (\see MessagePriority::Normal)
     template<class ...Args>
     void push(Args &&... args) {
         push(MessagePriority::Normal, std::forward<Args>(args)...);
     }
 
+    /// \brief Queue messages with explicit pre-defined \param priority.
     template<class ...Args>
     void push(MessagePriority priority, Args &&... args) {
         std::unique_lock guard(_queueGuard);
@@ -61,6 +90,7 @@ public:
         conditionVariable.notify_one();
     }
 
+    /// \brief Queue messages with arbitrary \param priority.
     template<class ...Args>
     void push(unsigned priority, Args &&... args) {
         std::unique_lock guard(_queueGuard);
@@ -68,6 +98,8 @@ public:
         conditionVariable.notify_one();
     }
 
+    /// \brief Retreive the highest-priority message.
+    /// \returns The content of the message.
     MessageContent take() {
         std::unique_lock guard(_queueGuard);
 
@@ -76,11 +108,8 @@ public:
         return message.content;
     }
 
-    void pop() {
-        std::unique_lock guard(_queueGuard);
-    }
-
-    void waitForMessage() {
+    /// \brief Suspend the thread until the next message arrives.
+    void waitForMessage() const {
         std::unique_lock guard(_queueGuard);
         conditionVariable.wait(guard, [this]() {
             return !empty();
