dfe09eadef4f38032b5c4a245f4b79a38a023d37
Dániel Molnár <mdinim@gmail.com>
2019-05-16 19:32:11 +0200

    Yet another refactor that hopefully does the job

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -29,7 +29,8 @@ private:
     template<class Callable, class ...Args>
     using ResultTypeOfCallable = typename std::invoke_result<Callable, Args...>::type;
 
-    using WrappedJob = std::function<void(unsigned)>;
+    using WrappedJob = std::function<void()>;
+    using JobQueue = std::queue<WrappedJob>;
 
     /// \brief Internal representation of a worker thread.
     class Worker {
@@ -37,22 +38,50 @@ private:
         /// \brief The wrapped thread.
         std::thread _thread;
 
-        /// \brief Flag that signals if the worker is occupied.
-        std::atomic_bool _occupied = false;
+        /// \brief Atomic flag for Worker stopping.
+        std::shared_ptr<std::atomic_bool> _stopped = std::make_shared<std::atomic_bool>(false);
+
+        /// \brief Static loop function that actually is executed on the worker threads
+        static void mainLoop (std::shared_ptr<std::atomic_bool> stopped, std::shared_ptr<JobQueue> queue,
+                    std::shared_ptr<std::shared_mutex> queueGuard,
+                    std::shared_ptr<std::condition_variable_any> hasJobOrStopped) {
+            while(!*stopped) {
+                std::optional<WrappedJob> job;
+                {
+                    std::unique_lock queueLock(*queueGuard);
+                    hasJobOrStopped->wait(queueLock, [&stopped, &queue]() {
+                        return *stopped || !queue->empty();
+                    });
+
+                    if (*stopped) {
+                        return;
+                    }
+
+                    if (!queue->empty()) {
+                        job = std::move(queue->front());
+                        queue->pop();
+                    }
+                }
+
+                if(job.has_value())
+                    job.value()();
+            }
+        }
     public:
         /// \brief Construct an empty worker (with nothing to do)
-        Worker() = default;
+        Worker(std::shared_ptr<JobQueue> queue, std::shared_ptr<std::shared_mutex> queueGuard,
+                std::shared_ptr<std::condition_variable_any> hasJobOrStopped)
+        {
+            _thread = std::thread(std::bind(&Worker::mainLoop, _stopped, queue, queueGuard, hasJobOrStopped));
+        }
 
         /// \brief Destruct the instance of the worker
         /// Detaches the underlying thread.
         ~Worker() {
+            stop();
             if(_thread.joinable())
                 _thread.detach();
         }
-
-        /// \brief Construct a worker with a job to do.
-        /// The thread is occupied afterwards.
-        explicit Worker(std::function<void()> job) noexcept : _thread(job), _occupied(true) {}
 
         /// \brief Disabled copy constructor.
         Worker(const Worker&) = delete;
@@ -60,158 +89,61 @@ private:
         /// \brief Disabled copy-assignment operator.
         Worker& operator=(const Worker&) = delete;
 
-        /// \brief Move constructor.
-        /// Takes over the ownership of the \param other Worker.
-        /// Finishes its current job first.
-        Worker(Worker&& other) noexcept {
-            if(_thread.joinable())
-                _thread.join();
+        /// \brief Disabled move constructor.
+        Worker(Worker&& other) = delete;
 
-            _thread = std::move(other._thread);
-            _occupied = other._occupied.load();
-            other._occupied = false;
-        }
+        /// \brief Disabled move assignment operator.
+        Worker& operator=(Worker&& other) = delete;
 
-        /// \brief Move assignment operator.
-        /// Same as move constructor.
-        Worker& operator=(Worker&& other) {
-            if(_thread.joinable())
-                _thread.join();
-
-            _thread = std::move(other._thread);
-            _occupied = other._occupied.load();
-            other._occupied = false;
-            return *this;
-        }
-        
-        /// \brief Lock the worker.
-        void lock() {
-            _occupied = true;
-        }
-
-        /// \brief Free the worker.
-        void free() {
-            _occupied = false;
-        }
-
-        /// \brief Check if the worker is occupied.
-        bool isFree() const {
-            return !_occupied;
+        /// \brief Stop the worker.
+        void stop() {
+            *_stopped = true;
         }
     };
 
-    /// \brief Guard for the worker array.
-    mutable std::mutex _workerGuard;
-
     /// \brief Container for the Workers.
-    std::array<Worker, N> _workers;
+    std::vector<std::unique_ptr<Worker>> _workers;
 
     /// \brief Guard for the job queue.
-    mutable std::shared_mutex _queueGuard;
-
-    /// \brief Atomic flag for ThreadPool stopping.
-    std::atomic_bool _stopped = false;
-
-    /// \brief Atomic flag if a stopped ThreadPool should finish its already queued jobs.
-    std::atomic_bool _waitForFinish = true;
+    std::shared_ptr<std::shared_mutex> _queueGuard = std::make_shared<std::shared_mutex>();
 
     /// \brief Container for the job queues.
-    std::queue<std::function<void(unsigned)>> _jobQueue;
+    std::shared_ptr<JobQueue> _jobQueue = std::make_shared<JobQueue>();
 
-    /// \brief The ThreadPool's own thread it dispatches the jobs on.
-    std::thread _poolingThread;
 
     /// \brief Condition variable to wait for stopping or jobs.
-    std::condition_variable_any hasJobOrStopped;
-
-    /// \brief Condition variable to wait finishing workers.
-    std::condition_variable hasUnoccupiedWorkerOrStopped;
-
-    /// \brief Retreive the next job to do.
-    std::optional<WrappedJob> takeNextJob() {
-        if (_jobQueue.empty()) {
-            return std::nullopt;
-        }
-        
-        WrappedJob job = std::move(_jobQueue.front());
-        _jobQueue.pop();
-
-        return job;
-    }
-
-    /// \brief Retreive the index of an idling Worker.
-    std::optional<unsigned> getIdleWorker() const {
-        auto workerIt = std::find_if(_workers.begin(), _workers.end(), [](const auto &worker) {
-            return worker.isFree();
-        });
-
-        return workerIt != _workers.end() ?
-            std::optional<unsigned>(std::distance(_workers.begin(), workerIt)) : std::nullopt;
-    }
-    
-    bool shouldStop() const {
-        std::shared_lock queueLock(_queueGuard);
-        return unsafeShouldStop();
-    }
-    
-    bool unsafeShouldStop() const {
-        return _stopped && (_jobQueue.empty() || !_waitForFinish);
-    }
-
-    /// \brief Main loop that does the orchestration.
-    void mainLoop() {
-        while (!shouldStop()) {
-            std::unique_lock workerLock(_workerGuard);
-            hasUnoccupiedWorkerOrStopped.wait(workerLock,
-                                          [this](){ return getIdleWorker().has_value() || unsafeShouldStop(); });
-            if(shouldStop())
-                continue;
-            
-            auto workerIndex = getIdleWorker();
-            
-            std::unique_lock queueLock(_queueGuard);
-            hasJobOrStopped.wait(queueLock, [this](){ return !_jobQueue.empty() || unsafeShouldStop(); });
-            if(unsafeShouldStop())
-                continue;
-            
-            auto job = takeNextJob();
-            _workers.at(workerIndex.value()) = Worker(std::bind(job.value(), workerIndex.value()));
-        }
-        
-        for (auto &worker : _workers) {
-            worker.free();
-        }
-    }
+    std::shared_ptr<std::condition_variable_any> hasJobOrStopped = std::make_shared<std::condition_variable_any>();
 public:
     /// \brief Construct the thread pool.
     /// Launches its pooling thread.
     ThreadPool() {
-        _poolingThread = std::thread(std::bind(&ThreadPool::mainLoop, this));
+        _workers.reserve(N);
+        for(unsigned i = 0; i < N; ++i) {
+            _workers.emplace_back(std::make_unique<Worker>(_jobQueue, _queueGuard, hasJobOrStopped));
+        }
     }
 
     /// \brief Destruct the thread pool instance.
     /// Does not wait for its Workers to finish, does not clears its job queue first.
     ~ThreadPool() {
-        stop(false);
-        if(_poolingThread.joinable())
-            _poolingThread.join();
+        _workers.clear();
+        hasJobOrStopped->notify_all();
     }
 
     /// \brief Stop the thread pool.
     /// \param waitForQueuedJobs decides whether or not clear ist job queue before finishing.
     void stop(bool waitForQueuedJobs) {
-        _stopped = true;
-        _waitForFinish = waitForQueuedJobs;
+        for (const auto& worker : _workers) {
+            worker->stop();
+        }
 
-        std::unique_lock queueLock(_queueGuard); // Wait for the loop to start waiting for the notification
-        hasUnoccupiedWorkerOrStopped.notify_one();
-        hasJobOrStopped.notify_one();
+        hasJobOrStopped->notify_all();
     }
 
     /// \brief Checks if it has any job waiting for a Worker.
     bool hasQueuedJob() const {
-        std::shared_lock lock(_queueGuard);
-        return !_jobQueue.empty();
+        std::shared_lock lock(*_queueGuard);
+        return !_jobQueue->empty();
     }
 
     /// \brief Add a job.
@@ -222,18 +154,15 @@ public:
     auto addJob(Callable job, Args ...args) -> std::future<ResultTypeOfCallable<Callable, Args...>> {
         auto task = std::make_shared<std::packaged_task<ResultTypeOfCallable<Callable, Args...>(Args...)>>(job);
 
-        auto wrappedJob = [this, task, args = std::make_tuple(std::forward<Args>(args)...)](unsigned idx) {
+        auto wrappedJob = [this, task, args = std::make_tuple(std::forward<Args>(args)...)]() {
             std::apply(*task, args);
-
-            _workers.at(idx).free();
-            hasUnoccupiedWorkerOrStopped.notify_one();
         };
 
         {
-            std::unique_lock lock(_queueGuard);
-            _jobQueue.push(wrappedJob);
+            std::unique_lock lock(*_queueGuard);
+            _jobQueue->push(wrappedJob);
         }
-        hasJobOrStopped.notify_one();
+        hasJobOrStopped->notify_one();
 
         return task->get_future();
     }
